7ff0a9fefa30190fa404edd4559fae96fdcc12d8
Wyatt Kirby <wyatt@apsis.io>
2020-06-06 15:09:08 -0700

    Add async fetch of trello data

--- pages/estimate.js
+++ pages/estimate.js
@@ -1,11 +1,20 @@
 /* global TrelloPowerUp */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { withTrello } from "../src/withTrello";
 
 const EstimatePage = ({ trello, t }) => {
-  console.log(trello, t.getAll());
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetch = async () => {
+      const allData = await t.getAll();
+      setData(allData);
+    };
 
+    fetch();
+  }, [t])
+
+  console.log(data);
   return <div>Estimate!</div>;
 };
 
