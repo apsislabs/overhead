ed8d6bba03ddfffc93d6ab23e67d4827897b7ff8
Wyatt Kirby <wyatt@apsis.io>
2020-06-06 17:47:49 -0700

    Switch from useMemo to useCallback

--- src/withTrello.js
+++ src/withTrello.js
@@ -1,10 +1,10 @@
-import React, { useMemo } from "react";
+import React, { useCallback } from "react";
 
 export const withTrello = (Component) => ({ trello, ...rest }) => {
   if (!trello) {
     return "Waiting for Trello...";
   }
 
-  const t = useMemo(() => trello.iframe(), [trello]);
-  return <Component trello={trello} t={t} {...rest} />;
+  const t = useCallback(trello.iframe, [trello]);
+  return <Component trello={trello} t={t()} {...rest} />;
 };
