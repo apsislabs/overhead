46718e9cf3b3b8dd5ca2149f14d0325c94ec45a4
Wyatt Kirby <wyatt@apsis.io>
2020-10-07 15:33:02 -0400

    Attempt naive calculation

--- src/calculators/calculateDistributions.js
+++ src/calculators/calculateDistributions.js
@@ -4,7 +4,32 @@ export const calculateHoursByDueDate = (cards, estimates) => {
   const e =  _.reduce(
     cards,
     (acc, card) => {
-      console.log(card);
+      console.log(card.due, card.dueComplete, card.dateLastActivity);
+
+      if ( !_.has(estimates, card.id)) {
+        return acc;
+      }
+
+      const estimate = parseFloat(_.get(estimates, card.id));
+
+      if (_.isNaN(estimate)) {
+        return acc;
+      }
+
+      if (!card.due) {
+        if (!_.has(acc, "noDeadline")) {
+          _.set(acc, "noDeadline", 0);
+        }
+
+        acc["noDeadline"] += estimate;
+      } else {
+        if(!_.has(acc, card.due)) {
+          _.set(acc, card.due, 0);
+        }
+
+        acc[card.due] += estimate;
+      }
+
       return acc;
     },
     {}
