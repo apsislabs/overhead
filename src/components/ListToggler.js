43743669144f2f59d656903321df4d9985d2f77b
Wyatt Kirby <wyatt@apsis.io>
2020-10-06 22:35:46 -0400

    Fix list toggler

--- src/components/ListToggler.js
+++ src/components/ListToggler.js
@@ -16,23 +16,22 @@ export const ListToggler = ({ label = "Edit Lists", lists, onToggle }) => {
       </button>
 
       <Collapse isOpened={open}>
-        <ListToggler lists={lists} onToggle={toggleListExclusion} />
+        <div className="pop-over-section">
+          <h4>Excluded Lists</h4>
+          <fieldset style={{ marginBottom: 0 }}>
+            {_.map(lists, (l) => {
+              return (
+                <CheckboxRow
+                  key={l.id}
+                  checked={excludedLists.indexOf(l.id) > -1}
+                  onChange={() => onToggle(l.id)}
+                  label={l.name}
+                />
+              );
+            })}
+          </fieldset>
+        </div>
       </Collapse>
-
-      <div className="pop-over-section">
-        <h4>Excluded Lists</h4>
-        <fieldset style={{ marginBottom: 0 }}>
-          {_.map(lists, (l) => {
-            return (
-              <CheckboxRow
-                key={l.id}
-                checked={excludedLists.indexOf(l.id) > -1}
-                onChange={() => onToggle(l.id)}
-                label={l.name} />
-            );
-          })}
-        </fieldset>
-      </div>
     </>
   );
 };
