dc3f7526c45f03b681d702ab0b33777b1862afd4
Wyatt Kirby <wyatt@apsis.io>
2020-10-06 22:37:56 -0400

    Extract toggle component

---
+++ src/components/CollapseButton.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Collapse } from "react-collapse";
+
+export const CollapseButton = ({ label, children }) => {
+  const [open, setOpen] = useState(false);
+
+  <>
+    <button
+      style={{ width: "100%", marginBottom: open ? 8 : 0 }}
+      onClick={() => setOpen(!open)}
+    >
+      {label}
+    </button>
+
+    <Collapse isOpened={open}>
+      <div className="pop-over-section">{children}</div>
+    </Collapse>
+  </>;
+};
--- src/components/ListToggler.js
+++ src/components/ListToggler.js
@@ -1,37 +1,26 @@
 import _ from "lodash";
-import React, { useState } from "react";
-import { Collapse } from "react-collapse";
+import React from "react";
 import { CheckboxRow } from "./CheckboxRow";
+import { CollapseButton } from "./CollapseButton";
 
 export const ListToggler = ({ label = "Edit Lists", lists, onToggle }) => {
-  const [open, setOpen] = useState(false);
-
   return (
     <>
-      <button
-        style={{ width: "100%", marginBottom: open ? 8 : 0 }}
-        onClick={() => setOpen(!open)}
-      >
-        {label}
-      </button>
-
-      <Collapse isOpened={open}>
-        <div className="pop-over-section">
-          <h4>Excluded Lists</h4>
-          <fieldset style={{ marginBottom: 0 }}>
-            {_.map(lists, (l) => {
-              return (
-                <CheckboxRow
-                  key={l.id}
-                  checked={excludedLists.indexOf(l.id) > -1}
-                  onChange={() => onToggle(l.id)}
-                  label={l.name}
-                />
-              );
-            })}
-          </fieldset>
-        </div>
-      </Collapse>
+      <CollapseButton label={label}>
+        <h4>Excluded Lists</h4>
+        <fieldset style={{ marginBottom: 0 }}>
+          {_.map(lists, (l) => {
+            return (
+              <CheckboxRow
+                key={l.id}
+                checked={excludedLists.indexOf(l.id) > -1}
+                onChange={() => onToggle(l.id)}
+                label={l.name}
+              />
+            );
+          })}
+        </fieldset>
+      </CollapseButton>
     </>
   );
 };
