8b5208434823617f7ae8475b16e89b0fc807b64a
Wyatt Kirby <wyatt@apsis.io>
2020-10-07 15:22:06 -0400

    Fix which page has the new stuff

--- pages/breakdowns.js
+++ pages/breakdowns.js
@@ -1,145 +1,28 @@
 /* global TrelloPowerUp */
 
-import _ from "lodash";
-import React, { useEffect, useRef, useState } from "react";
-import { Collapse } from "react-collapse";
-import { CheckboxRow } from "../src/components/CheckboxRow";
-import { EstimateRow } from "../src/components/EstimateRow";
+import React from "react";
+import { calculateHoursByDueDate } from "../src/calculators/calculateDistributions";
 import { Loader } from "../src/components/Loader";
+import { useTrello } from "../src/contexts/TrelloContext";
 import { useTrelloData } from "../src/hooks/useTrelloData";
 import { withTrello } from "../src/withTrello";
 
-const calculateDistributions = (cards, estimates, excludedLists = []) => {
-  return _.reduce(
-    cards,
-    (acc, card) => {
-      if (
-        excludedLists.indexOf(card.idList) > -1 ||
-        !_.has(estimates, card.id)
-      ) {
-        return acc;
-      }
+const BreakdownsPage = () => {
+  const { trello, resize } = useTrello();
+  const { trelloData, toggleListExclusion } = useTrelloData(trello);
 
-      const estimate = parseFloat(_.get(estimates, card.id));
-      if (_.isNaN(estimate)) {
-        return acc;
-      }
-
-      if (card.members.length < 1) {
-        if (!_.has(acc, "unassigned")) {
-          _.set(acc, "unassigned", 0);
-        }
-
-        acc["unassigned"] += estimate;
-      }
-
-      _.forEach(card.members, (member) => {
-        if (!_.has(acc, member.id)) {
-          _.set(acc, member.id, 0);
-        }
-
-        acc[member.id] += estimate;
-      });
-
-      return acc;
-    },
-    {}
-  );
-};
-
-const BreakdownsPage = ({ t }) => {
-  const rootEl = useRef(null);
-  const [open, setOpen] = useState(false);
-  const [state, dispatch] = useTrelloData();
-  const { loading, members, lists, cards, estimates, excludedLists } = state;
-
-  const estimateTotals = calculateDistributions(
+  const {
+    loading,
+    members,
+    lists,
     cards,
     estimates,
-    excludedLists
-  );
+    excludedLists,
+  } = trelloData;
 
-  const { unassigned, ...teamTotals } = estimateTotals;
+  calculateHoursByDueDate(cards, estimates, excludedLists);
 
-  // Resize when recalculating the estimates
-  useEffect(() => {
-    if (rootEl.current) {
-      t.sizeTo(rootEl.current);
-    }
-  }, [rootEl.current, estimateTotals]);
-
-  // Store any change to excluded lists
-  useEffect(() => {
-    t.set("member", "private", "excludedLists", excludedLists);
-  }, [excludedLists]);
-
-  const handleToggle = (id) => {
-    if (excludedLists.indexOf(id) > -1) {
-      dispatch({ type: "excludeList", id });
-    } else {
-      dispatch({ type: "includeList", id });
-    }
-  };
-
-  return loading ? (
-    <Loader />
-  ) : (
-    <div ref={rootEl}>
-      {_.map(teamTotals, (e, memberId) => {
-        const member = _.find(members, (m) => m.id === memberId);
-        const name = _.get(member, "fullName", "Unassigned");
-        const avatarUrl = _.get(member, "avatar", null);
-
-        return (
-          <EstimateRow
-            key={memberId}
-            name={name}
-            avatarUrl={avatarUrl}
-            hours={e}
-          />
-        );
-      })}
-
-      <hr />
-
-      {typeof unassigned !== "undefined" && (
-        <div>
-          <strong>Unassigned:</strong> {unassigned} Hours
-        </div>
-      )}
-
-      {typeof teamTotals !== "undefined" && (
-        <div>
-          <strong>Total:</strong> {_.sum(Object.values(teamTotals))} Hours
-        </div>
-      )}
-
-      <button
-        style={{ width: "100%", marginBottom: 8 }}
-        onClick={() => setOpen(!open)}
-      >
-        Edit Lists
-      </button>
-
-      <Collapse isOpened={open}>
-        <div className="pop-over-section">
-          <h4>Excluded Lists</h4>
-          <fieldset style={{ marginBottom: 0 }}>
-            {_.map(lists, (l) => {
-              return (
-                <CheckboxRow
-                  key={l.id}
-                  checked={excludedLists.indexOf(l.id) > -1}
-                  onChange={() => handleToggle(l.id)}
-                  label={l.name}
-                />
-              );
-            })}
-          </fieldset>
-        </div>
-      </Collapse>
-    </div>
-  );
+  return <Loader />;
 };
 
 export default withTrello(BreakdownsPage);
--- pages/estimate.js
+++ pages/estimate.js
@@ -1,33 +1,99 @@
 /* global TrelloPowerUp */
 
-import React from "react";
-import { calculateHoursByDueDate } from "../src/calculators/calculateDistributions";
+import React, { useEffect, useRef, useState } from "react";
+import { useField, useForm } from "react-final-form-hooks";
 import { Loader } from "../src/components/Loader";
 import { useTrello } from "../src/contexts/TrelloContext";
-import { useTrelloData } from "../src/hooks/useTrelloData";
 import { withTrello } from "../src/withTrello";
 
-const DistributionPage = () => {
-  const { trello, resize } = useTrello();
-  const { trelloData, toggleListExclusion } = useTrelloData(trello);
-
-  const {
-    loading,
-    members,
-    lists,
-    cards,
-    estimates,
-    excludedLists,
-  } = trelloData;
-
-  calculateHoursByDueDate(cards, estimates);
-
-  // const { unassigned, ...teamTotals } = estimateTotals;
-
-  // // This page resizes when estimate totals changes
-  // useEffect(resize, [JSON.stringify(estimateTotals), loading]);
-
-  return <Loader />;
+const inputStyles = {
+  marginRight: 8,
+  marginBottom: 0,
+  width: "100%",
 };
 
-export default withTrello(DistributionPage);
+const buttonStyles = {
+  margin: 0,
+};
+
+const rowStyles = {
+  display: "flex",
+};
+
+const mustBeNumber = (value) => (isNaN(value) ? "Must be a number" : undefined);
+
+const EstimatePage = () => {
+  const { trello } = useTrello();
+  const rootEl = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [estimate, setEstimate] = useState("");
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        setLoading(true);
+        const storedEstimate = await trello.get(
+          "card",
+          "shared",
+          "estimate",
+          ""
+        );
+        setEstimate(storedEstimate);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetch();
+  }, [trello]);
+
+  useEffect(() => {
+    if (rootEl.current) {
+      trello.sizeTo(rootEl.current);
+    }
+  }, [rootEl.current]);
+
+  const onSubmit = async (values) => {
+    const { estimate } = values;
+    await trello.set("card", "shared", "estimate", estimate);
+    trello.closePopup();
+  };
+
+  const { form, handleSubmit, pristine, submitting } = useForm({
+    onSubmit,
+    initialValues: { estimate },
+  });
+
+  const estimateField = useField("estimate", form, mustBeNumber);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <div ref={rootEl}>
+      <form onSubmit={handleSubmit}>
+        <div style={rowStyles}>
+          <input
+            type="number"
+            autoFocus
+            style={inputStyles}
+            {...estimateField.input}
+            placeholder="Estimate"
+          />
+
+          <button
+            style={buttonStyles}
+            className="mod-primary"
+            type="submit"
+            disabled={pristine || submitting}
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default withTrello(EstimatePage);
