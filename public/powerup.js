a682ff186e46b49f569868eecb40d21f07ed2f53
Wyatt Kirby <wyatt@apsis.io>
2021-02-22 13:32:49 -0500

    Add post button

--- public/powerup.js
+++ public/powerup.js
@@ -16,6 +16,25 @@ const BreakdownPopup = (t) =>
     url: "breakdowns",
   });
 
+async function postData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
 TrelloPowerUp.initialize({
   // Start adding handlers for your capabilities here!
   "card-buttons": function (t, options) {
@@ -65,6 +84,15 @@ TrelloPowerUp.initialize({
       {
         text: "Breakdowns",
         callback: BreakdownPopup,
+      },
+      {
+        text: "Post",
+        callback: () => {
+          console.log("Notifying Slack!");
+          postData(
+            "https://kumh2i8hk7.execute-api.us-west-2.amazonaws.com/prod/sprint"
+          );
+        },
       },
     ];
   },
