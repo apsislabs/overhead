4f328d304370a5f1373c02f37757a7dd2e2fffd2
Wyatt Kirby <wyatt@apsis.io>
2021-02-22 12:18:58 -0500

    Add border to table style

--- src/components/EstimateTable.js
+++ src/components/EstimateTable.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const EstimateTable = ({ children }) => {
   return (
-    <table style="width: 100%;">
+    <table style={{ width: "100%", borderWidth: 0 }}>
       <tbody>{children}</tbody>
     </table>
   );
