d91b9517268d9a0950c1f115436e640f3a29d374
Wyatt Kirby <wyatt@apsis.io>
2021-02-23 13:26:37 -0500

    Point slack button at localhost

--- src/utils/postData.js
+++ src/utils/postData.js
@@ -1,7 +1,8 @@
-export const SHAMEBOT_URL =
-  "https://kumh2i8hk7.execute-api.us-west-2.amazonaws.com/prod/sprint";
+export const SHAMEBOT_URL = "http://localhost:3000/prod/shame";
+// export const SHAMEBOT_URL =
+//   "https://kumh2i8hk7.execute-api.us-west-2.amazonaws.com/prod/sprint";
 
-export const postData= async (url = "", data = {}) => {
+export const postData = async (url = "", data = {}) => {
   const response = await fetch(url, {
     method: "POST",
     mode: "no-cors",
@@ -16,4 +17,4 @@ export const postData= async (url = "", data = {}) => {
   });
 
   return response.json();
-}
+};
